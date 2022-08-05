ef54999c73f87ab838e3c0aa80a76437f5218f6e
soohunee <tngjsdl036@gmail.com>
2022-08-05 17:13:18 +0900

    Add Functionalities

---
+++ src/main/java/com/security/practice/demo/config/AppConfiguration.java
@@ -0,0 +1,15 @@
+package com.security.practice.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AppConfiguration {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/security/practice/demo/repository/UserRepository.java
+++ src/main/java/com/security/practice/demo/repository/UserRepository.java
@@ -4,6 +4,5 @@ import com.security.practice.demo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
     User findByUsername(String username);
 }
